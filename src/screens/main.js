97c53d0a016f6cb1079dc7d5ca8b850bf6a1b26c
gauravkr2506 <gauravkr2506@gmail.com>
2019-01-30 12:34:24 +0530

    image setting

--- src/screens/main.js
+++ src/screens/main.js
@@ -1,6 +1,6 @@
 import React from "react";
 import SplashScreen from "react-native-splash-screen";
-import { FlatList, View, Text, Image, TouchableHighlight, ScrollView } from "react-native";
+import { FlatList, View, Text, Image, TouchableHighlight, TouchableNativeFeedback, ScrollView } from "react-native";
 
 import MyListItem from "./../components/main/main-list";
 
@@ -21,29 +21,72 @@ export default class Main extends React.Component {
 	_renderItem = ({ item }) => <MyListItem data={item} navigation={this.props.navigation} />;
 	render() {
 		return (
-			<ScrollView contentContainerStyle={{ margin: 0, padding: 0, backgroundColor: "#ccc" }}>
-				<View>
-					<View style={{ flexDirection: "row" }}>
-						<TouchableHighlight onPress={() => this.showWebPage("https://www.amarujala.com/", "Amar Ujala")} style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-						<View style={{ backgroundColor: "red", margin: 3, padding: 2 }}>
-							<View style={{alignItems: "center", justifyContent: "center" }}>
-								<Image source={require("./../../img/1.jpg")} resizeMode="contain" style={{ width: 100, height: 53 }} />
-							</View>
-							<View style={{backgroundColor:"orange",width:100}}><Text style={{textAlign:"center"}}>Amar Unala</Text></View>
+			<View style={{ flex: 1, backgroundColor: "#ccc" }}>
+				<ScrollView contentContainerStyle={{ margin: 0, padding: 0, backgroundColor: "#ccc", flex: 0 }}>
+					<View>
+						<View style={{ flexDirection: "row", alignItem: "center", justifyContent: "center" }}>
+							<TouchableNativeFeedback style={{ flex: 1 }}>
+								<View style={{ flex: 1, height: 130, alignItems: "center", justifyContent: "center", marginVertical: 3 }}>
+									<View style={{ borderWidth: 2, backgroundColor: "red", borderColor: "red", width: 110, height: 110, alignItems: "center", justifyContent: "center" }}>
+										<Image source={require("./../../img/1.jpg")} resizeMode="center" style={{ width: 106 }} />
+									</View>
+									<View style={{ backgroundColor: "orange", height: 26, borderWidth: 2, borderColor: "red", width: 110, alignItems: "center", justifyContent: "center" }}>
+										<Text numberOfLines={1}>Item 1</Text>
+									</View>
+								</View>
+							</TouchableNativeFeedback>
+							<TouchableNativeFeedback style={{ flex: 1 }}>
+								<View style={{ flex: 1, height: 130, alignItems: "center", justifyContent: "center", marginVertical: 3 }}>
+									<View style={{ borderWidth: 2, backgroundColor: "red", borderColor: "red", width: 110, height: 110, alignItems: "center", justifyContent: "center" }}>
+										<Image source={require("./../../img/1.jpg")} resizeMode="center" style={{ width: 106 }} />
+									</View>
+									<View style={{ backgroundColor: "orange", height: 26, borderWidth: 2, borderColor: "red", width: 110, alignItems: "center", justifyContent: "center" }}>
+										<Text numberOfLines={1}>Item 1</Text>
+									</View>
+								</View>
+							</TouchableNativeFeedback>
+							<TouchableNativeFeedback style={{ flex: 1 }}>
+								<View style={{ flex: 1, height: 130, alignItems: "center", justifyContent: "center", marginVertical: 3 }}>
+									<View style={{ borderWidth: 2, backgroundColor: "red", borderColor: "red", width: 110, height: 110, alignItems: "center", justifyContent: "center" }}>
+										<Image source={require("./../../img/1.jpg")} resizeMode="center" style={{ width: 106 }} />
+									</View>
+									<View style={{ backgroundColor: "orange", height: 26, borderWidth: 2, borderColor: "red", width: 110, alignItems: "center", justifyContent: "center" }}>
+										<Text numberOfLines={1}>Item 1</Text>
+									</View>
+								</View>
+							</TouchableNativeFeedback>
 						</View>
-						</TouchableHighlight>
-						<TouchableHighlight onPress={() => this.showWebPage("https://www.jagran.com/", "Danik Jagran")}>
-						<View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "red", margin: 3, padding: 1 }}>
-								<Image source={require("./../../img/2.jpg")} resizeMode="contain" style={{ width: 100, height: 53 }} />
+						{/* <View style={{ flexDirection: "row" }}>
+						<TouchableHighlight onPress={() => this.showWebPage("https://www.jagran.com/", "Danik Jagran")} style={{ flex: 1 }}>
+							<View style={{ backgroundColor: "red", margin: 3, flexDirection: "column" }}>
+								<View style={{ alignItems: "center", justifyContent: "center", width: "100%" }}>
+									<Image source={require("./../../img/1.jpg")} resizeMode="center" />
+								</View>
+								<View style={{ backgroundColor: "orange" }}>
+									<Text style={{ textAlign: "center" }}>Amar Unala</Text>
+								</View>
 							</View>
 						</TouchableHighlight>
-						<TouchableHighlight onPress={() => this.showWebPage("https://www.bbc.com/hindi", "BBC")}>
-						<View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "red", margin: 3, padding: 1 }}>
-								<Image source={require("./../../img/3.jpg")} resizeMode="contain" style={{ width: 100, height: 53 }} />
+						<TouchableHighlight onPress={() => this.showWebPage("https://www.jagran.com/", "Danik Jagran")} style={{ flex: 1 }}>
+							<View style={{ backgroundColor: "red", margin: 3, flexDirection: "column" }}>
+								
+								<View style={{ backgroundColor: "orange" }}>
+									<Text style={{ textAlign: "center" }}>Amar Unala</Text>
+								</View>
 							</View>
 						</TouchableHighlight>
-					</View>
-					{/* <View style={{ flexDirection: "row" }}>
+						<TouchableHighlight onPress={() => this.showWebPage("https://www.bbc.com/hindi", "BBC")} style={{ flex: 1 }}>
+							<View style={{ backgroundColor: "red", margin: 3, flexDirection: "column" }}>
+								<View style={{ alignItems: "center", justifyContent: "center" }}>
+									<Image source={require("./../../img/1.jpg")} resizeMode="center" />
+								</View>
+								<View style={{ backgroundColor: "orange" }}>
+									<Text style={{ textAlign: "center" }}>Amar Unala</Text>
+								</View>
+							</View>
+						</TouchableHighlight>
+					</View> */}
+						{/* <View style={{ flexDirection: "row" }}>
 						<View style={{ flex: 1, padding: 10, alignItems: "center", justifyContent: "center" }}>
 							<TouchableHighlight onPress={() => this.showWebPage("https://www.bbc.com/hindi", "BBC")}>
 								<Image source={require("./../../img/3.jpg")} resizeMode="contain" style={{ width: 160 }} />
@@ -79,8 +122,9 @@ export default class Main extends React.Component {
 							</TouchableHighlight>
 						</View>
 					</View> */}
-				</View>
-			</ScrollView>
+					</View>
+				</ScrollView>
+			</View>
 		);
 	}
 }
